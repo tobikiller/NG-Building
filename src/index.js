40904b016907d1ec4d249cf82b65b8471675e0c6
tobikiller <edwardnguimeya@yahoo.com>
2022-03-19 00:42:46 +0100

    navtoggle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const toggleBtn =  document.getElementById("toggle-btn")
 const navLink = document.getElementById("nav-link")
+let first = document.getElementById("toggle-btn").firstElementChild
+let last = document.getElementById("toggle-btn").lastElementChild
+const middle = document.querySelector(".middle")
+
+console.log(first)
 
 //Toggle menu
 
 toggleBtn.addEventListener("click", function() {
     navLink.classList.toggle("open")
+    first.classList.toggle("top")
+    last.classList.toggle("down")
+    middle.classList.toggle("inmiddle")
 
 })
